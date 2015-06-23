b409891b1b08aae36199285c3e13c9e7cb497481
AmitaiB <blickstein@gmail.com>
2015-06-23 14:03:02 -0400

    adds images to images.xcassets

--- FlashyTableViews/FISFlashyTableViewController.m
+++ FlashyTableViews/FISFlashyTableViewController.m
@@ -6,8 +6,9 @@
 //  Copyright (c) 2015 Amitai's school of One. All rights reserved.
 //
 
-#import <SWTableViewCell.h>
 #import "FISFlashyTableViewController.h"
+#import <SWTableViewCell.h>
+
 
 @interface FISFlashyTableViewController ()
 
@@ -33,33 +34,28 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return 10;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *cellIdentifier = @"swCellReuseID";
     
-    SWTableViewCell *cell = (SWTableViewCell *)[tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    SWTableViewCell *cell = (SWTableViewCell *)[tableView dequeueReusableCellWithIdentifier:cellIdentifier
+                                                                               forIndexPath:indexPath];
     
-    if (cell == nil) {
-        cell = [[SWTableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
-        cell.leftUtilityButtons = [self leftButtons];
-        cell.rightUtilityButtons = [self rightButtons];
-        cell.delegate = self;
-    }
+    cell.leftUtilityButtons = [self leftButtons];
+    cell.rightUtilityButtons = [self rightButtons];
+//    cell.delegate = self;
     
-    NSDate *dateObject = _testArray[indexPath.row];
-    cell.textLabel.text = [dateObject description];
-    cell.detailTextLabel.text = @"Some detail text";
-    
+    cell.textLabel.text = @"Some Text";
+    cell.imageView.image = [UIImage imageNamed:@"MyAwesomeTableCellImage"];
+//    [cell setCellHeight:cell.frame.size.height];
     return cell;
 }
 
