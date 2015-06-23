efe320bc3166eac01d2dc85af41c7d4fdf0871e1
AmitaiB <blickstein@gmail.com>
2015-06-23 13:37:13 -0400

    Initial commit

---
+++ FlashyTableViews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlashyTableViews
+//
+//  Created by Amitai Blickstein on 6/23/15.
+//  Copyright (c) 2015 Amitai's school of One. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlashyTableViews/FISFlashyTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISFlashyTableViewController.h
+//  FlashyTableViews
+//
+//  Created by Amitai Blickstein on 6/23/15.
+//  Copyright (c) 2015 Amitai's school of One. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISFlashyTableViewController : UITableViewController
+
+@end
