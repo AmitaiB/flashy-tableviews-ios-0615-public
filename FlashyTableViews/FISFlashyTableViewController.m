de5335df95d8d3d1aa1443e8fa1fdaa16847ffbe
AmitaiB <blickstein@gmail.com>
2015-06-23 23:44:26 -0400

    PowderToastMan fully visible

--- FlashyTableViews/FISFlashyTableViewController.m
+++ FlashyTableViews/FISFlashyTableViewController.m
@@ -25,7 +25,7 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    [self.tableView addParallaxWithImage:[UIImage imageNamed:@"powderToastMan"] andHeight:160];
+    [self.tableView addParallaxWithImage:[UIImage imageNamed:@"powderToastMan"] andHeight:460];
 
 }
 
